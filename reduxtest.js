940bfe47ce1954cf104a27b667ea310f4d9e72f4
Kalpatil <48372766+Kalpatil@users.noreply.github.com>
2020-02-10 12:20:47 +0530

    Add files via upload

---
+++ reduxtest.js
@@ -0,0 +1,35 @@
+const { createStore } = require('redux');
+
+const newState = {
+    age : 21
+};
+
+
+const myReducer = ( state = newState, action ) => {
+    
+    const newState = {...state};
+    if( action.type === 'ADD' ) {
+        newState.age += action.val;
+    }
+
+    if( action.type === 'SUBSTRACT') {
+        newState.age -= action.val;
+    }
+
+    return newState;
+}
+
+const store = createStore(myReducer);
+
+store.subscribe (()=> {
+    console.log('Satte Changed' + JSON.stringify( store.getState() ));
+})
+// action
+//console.log('Before add' + JSON.stringify( store.getState() ));
+
+store.dispatch({type:'ADD', val :10});
+//console.log('After add' + JSON.stringify( store.getState() ));
+
+store.dispatch({type:'SUBSTRACT', val : 5});
+
+//console.log('After SUBSTRACT' + JSON.stringify( store.getState()));
